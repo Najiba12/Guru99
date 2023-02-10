a3e2051f3aab5ae40eafe45770da6d789025b234
Najiba <najibazahra6@gmail.com>
2023-02-10 22:36:14 +0500

    initial commit

--- cumcumber2/src/test/java/StepDefinition/Steps.java
+++ cumcumber2/src/test/java/StepDefinition/Steps.java
@@ -1,10 +1,12 @@
 package StepDefinition;
 
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -19,19 +21,33 @@ public class Steps {
 	       driver= new FirefoxDriver();					
 	       driver.manage().window().maximize();			
 	       driver.get("http://demo.guru99.com/v4");		
-	       
+	     //  String url=driver.getTitle();
+	   	Assert.assertEquals("They are same",driver.getTitle(), "Guru99 Bank Home Page");
+	   	
 	    }		
 
 	    @When("^Enter the Username and Password$")					
 	    public void enter_the_Username_and_Password() throws Throwable 							
 	    {		
 	       driver.findElement(By.name("uid")).sendKeys("username12");							
-	       driver.findElement(By.name("password")).sendKeys("password12");							
-	    }		
-
+	       driver.findElement(By.name("password")).sendKeys("password12");		
+	       Thread.sleep(3000);
+	    }
+	  
 	    @Then("^Reset the credential$")					
 	    public void Reset_the_credential() throws Throwable 							
 	    {		
-	       driver.findElement(By.name("btnReset")).click();					
+	       driver.findElement(By.name("btnLogin")).click();					
 	    }	
+	    @And("^accept the alert$")
+	    public void accept_the_alert() throws Throwable{
+	    	Alert alert = driver.switchTo().alert();
+	    	alert.accept();
+	    }
+        @Then("^quit$")
+        public void quit() throws Throwable{
+        	Thread.sleep(2000);
+        	driver.quit();
+        }
+	    
 }
--- cumcumber2/src/test/resources/Features/MyTest.feature
+++ cumcumber2/src/test/resources/Features/MyTest.feature
@@ -4,3 +4,5 @@ Feature: Reset functionality on login page of Application
     Given Open the Firefox and launch the application
     When Enter the Username and Password
     Then Reset the credential
+     And accept the alert
+    Then quit
